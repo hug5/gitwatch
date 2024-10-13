ba28e8b520ded728f89e52552a5de3b7cd604751
hug5 <dhub2@gmx.com>
2024-10-13 00:20:08 -0700

    wip

--- gw-command.sh
+++ gw-command.sh
@@ -1,4 +1,5 @@
 ## Sample command file
+# // 2024-10-13
 
 # Run SASS
 SCSS="jug/www/static/scss/style.container.scss"
@@ -14,16 +15,22 @@ git commit --amend --no-edit
 git push --force
 
 
-#--------------
+#----------------------------------
 
 # Run remote commands on Tmux pane N (Whichever you elected);
 # Normally, assuming the remote pane is pane 0, in window 1, you would do:
 # $ tmux send-keys -t 1.0 "<some command>" enter
 # $ tmux send-keys -t 1.0 "git pull --rebase" enter
-# But as a shortcut, can replace the tmux specific statement with {remote}
+
+# Or use a variable to denote the window and pane:
+# $ tmux send-keys -t ${WINDOW}.${PANE} "git pull --rebase" enter
+# This is better as WINDOW and PANE variables will capture your flag options;
+
+# Or as a shortcut, can replace the tmux specific statement with {remote}
 # So you could do:
 # $ {remote} "git pull --rebase" enter
-
+# This would be the most concise syntax;
+# When gitwatch sees {remote}, it'll replace it with the full 'tmux-send-keys...' command.
 
 # Pull git:
 {remote} "git pull --rebase" enter
